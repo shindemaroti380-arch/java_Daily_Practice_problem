a730851a9f3d4ee99c338df5627238779042e2bd
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-14 23:54:30 +0530

    Update All_data_types.java

--- All_data_types.java
+++ All_data_types.java
@@ -6,7 +6,17 @@ public class All_data_types {
         char c = 'b';
         short s = 10000;
         boolean t = true;
+        float f = 1.2f;
         System.out.println("all data types");
+         System.out.println("byte = " + b);
+        System.out.println("short = " + s);
+        System.out.println("int = " + a);
+        System.out.println("long = " + l);
+        System.out.println("float = " + f);
+        
+        System.out.println("char = " + c);
+        System.out.println("boolean = " + t);
+
 
 
     }
