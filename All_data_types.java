5d3218cf1774979341e411bc3e5f7d682f188d91
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-14 23:50:02 +0530

    Create All_data_types.java

---
+++ All_data_types.java
@@ -0,0 +1,14 @@
+public class All_data_types {
+    public static void main(String[] args) {
+        int a = 20;
+        byte b = 100;
+        long l = 100000;
+        char c = 'b';
+        short s = 10000;
+        boolean t = true;
+        System.out.println("all data types");
+
+
+    }
+    
+}
