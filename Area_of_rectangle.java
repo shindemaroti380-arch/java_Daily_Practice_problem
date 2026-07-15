7aa8d0a9c4757f73819efdbe05b1a210660ea5e2
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-15 23:20:46 +0530

    Create Area_of_rectangle.java

---
+++ Area_of_rectangle.java
@@ -0,0 +1,9 @@
+public class Area_of_rectangle {
+    public static void main(String[] arrgs) {
+        int length = 20;
+        int width = 10;
+        int area = length * width;
+        System.out.println("area = " +area);
+    }
+    
+}
