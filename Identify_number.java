a615914a822de2fb0b56b5d5602a3fc67f595476
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-16 23:18:50 +0530

    Create Identify_number.java

---
+++ Identify_number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class identify_number {
+    public static void main(String[] args){
+        System.out.println("ennter an number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+
+    }
+    
+}
