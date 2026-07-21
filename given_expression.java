1e7261a61281e4e0cec42fe1f168e897761a8be4
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-21 23:07:54 +0530

    Create given_expression.java

---
+++ given_expression.java
@@ -0,0 +1,12 @@
+public class given_exprssion {
+    public static void main(String[] args){
+        float u = 10.0f;
+        float v = 20.0f;
+        float s = 30.0f;
+
+        float a = (v*v -u*u)/(2*s);
+        System.out.println(a);
+
+    }
+    
+}
