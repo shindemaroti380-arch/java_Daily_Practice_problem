4f505140e40bb1dc2f29aad1b52f3b4cd3d39425
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-21 23:34:56 +0530

    Create given_expressionassignment2.java

---
+++ given_expressionassignment2.java
@@ -0,0 +1,8 @@
+public class given_expression_chw5 {
+    public static void main(String[] args) {
+        int x = 7;
+        int a = 749 / 7 + 35 / 7;
+        System.out. println(a);
+    }
+    
+}
