00622aa62b28381033cd33fce5e22bde892af069
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 23:30:15 +0530

    Create ends_with_boolean.java

---
+++ ends_with_boolean.java
@@ -0,0 +1,11 @@
+public class ends_with_boolean {
+    public static void main(String[] args) {
+        String name = "maroti";
+        int value = name.length();
+        System.out.println(value);
+        System.out.println(name.endsWith("oti"));
+
+
+    }
+    
+}
