dc38e19bd0682c75fd4db51b60840f20d1dc25ca
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 22:37:07 +0530

    Create to_upper_case.java

---
+++ to_upper_case.java
@@ -0,0 +1,11 @@
+public class upper_case {
+    public static void main(String[] args) {
+        String name = "maroti";
+        int value = name.length();
+        String b1 = name.toUpperCase();
+        System.out.println(value);
+        System.out.println(b1);
+        
+    }
+    
+}
