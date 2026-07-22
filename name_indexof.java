9ba9a90de97d15bd0be62956f2053fb23f5ca847
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 23:41:57 +0530

    Create name_indexof.java

---
+++ name_indexof.java
@@ -0,0 +1,10 @@
+public class name_indexof {
+    public static void main(String[] args) {
+        String name = "Harry";
+        int value = name.length();
+        System.out.println(value);
+        System.out.println(name.indexOf('r'));
+
+    }
+    
+}
