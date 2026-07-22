b855ebb0c282b4962c3c9490ce4ae003707ed097
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 23:25:37 +0530

    Create boolean_name_start.java

---
+++ boolean_name_start.java
@@ -0,0 +1,8 @@
+public class StartsWithExample {
+    public static void main(String[] args) {
+        String name = "Maroti";
+
+        System.out.println(name.startsWith("Mar")); // true
+        System.out.println(name.startsWith("mar")); // false
+    }
+}
