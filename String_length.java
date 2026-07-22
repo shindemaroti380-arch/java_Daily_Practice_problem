88393f1cb639eeff4c8dfcc48b7ea27a727a86e1
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 22:20:55 +0530

    Create String_length.java

---
+++ String_length.java
@@ -0,0 +1,11 @@
+public class String_length {
+    public static void main(String[] args){
+       String name = "maroti";
+       int value = name.length();
+       System.out.println(value);
+
+
+
+    }
+    
+}
