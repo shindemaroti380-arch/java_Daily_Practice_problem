731969d934ca17251405b1db527715cfbbdd966a
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 22:33:43 +0530

    Create lower_case.java

---
+++ lower_case.java
@@ -0,0 +1,12 @@
+public class to_lowercase {
+    public static void main(String[] args) {
+        String name = "Maroti";
+        int value = name . length();
+        String a1= name.toLowerCase();
+        System.out.println(value);
+        System.out.println(a1);
+
+        
+    }
+    
+}
