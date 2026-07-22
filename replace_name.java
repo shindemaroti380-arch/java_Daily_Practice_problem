ad8e755e551b7a15d80870be4f92a4f25b5242d7
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 23:12:01 +0530

    Create replace_name.java

---
+++ replace_name.java
@@ -0,0 +1,11 @@
+public class replace {
+    public static void main(String[] args) {
+        String name = "Harry";
+        int value = name.length();
+        System.out.println(value);
+        System.out.println(name.replace('r','p'));
+        System.out.println(name.replace('r','p'));
+
+    }
+    
+}
