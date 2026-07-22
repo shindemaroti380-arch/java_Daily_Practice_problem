314a7872a8dd58a1ed7487a74e52e8992ea50809
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 23:35:51 +0530

    Create charAt.java

---
+++ charAt.java
@@ -0,0 +1,9 @@
+public class charat {
+    public static void main(String[] args) {
+        String name = "maroti";
+        int value = name.length();
+        System.out.println(value);
+        System.out.println(name.charAt(2));
+    }
+    
+}
