2c5b1ba8f29a948265c380c47365296b369ba70c
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 22:55:56 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,11 @@
+public class substring {
+    public static void main(String[] args) {
+        String name = "maroti";
+        int value = name.length();
+        System.out.println(value);
+        System.out.println(name.substring(1,4));
+
+        
+    }
+    
+}
