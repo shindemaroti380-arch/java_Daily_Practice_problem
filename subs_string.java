c638cf98e77b0d9e551052076ea40ffb4af05b53
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-22 23:00:28 +0530

    Create subs_string.java

---
+++ subs_string.java
@@ -0,0 +1,9 @@
+public class substring {
+    public static void main(String[] args) {
+        String name = "maroti";
+        int value = name.length();
+        System.out.println(value);
+        System.out.println(name.substring(5));
+    }
+    
+}
