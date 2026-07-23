5a13bd3470e12cd2306456ba93b5d6cb9777d29e
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-23 23:17:04 +0530

    Create age_if_else.java

---
+++ age_if_else.java
@@ -0,0 +1,13 @@
+public class age_contidional {
+    public static void main(String[] args){
+        int age = 28;
+        if(age>18){
+            System.out.println("yes boy you can drive!");
+
+        }
+        else{
+            System.out.println("no boy you cannot drive yet!");
+
+        }
+    }
+}    
