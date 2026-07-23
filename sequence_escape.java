6248f7183e32f2033f9612f2a202f15da2c07004
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-23 23:01:00 +0530

    Create sequence_escape.java

---
+++ sequence_escape.java
@@ -0,0 +1,8 @@
+public class escape_sequence {
+    public static void main(String[] args){
+        String myLetter = "\nDear maroti , \nthis java course is nice, \nthanks ";
+        System.out.println(myLetter);
+    }
+
+    
+}
