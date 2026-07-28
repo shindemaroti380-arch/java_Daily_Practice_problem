31429b9ab117d050ef7be0ba370548b482d52236
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-28 23:20:20 +0530

    Create print_odd_number.java

---
+++ print_odd_number.java
@@ -0,0 +1,10 @@
+ublic class print_odd_number {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=50){
+            System.out.println(i);
+            i+= 2;
+        }
+    }
+    
+}
