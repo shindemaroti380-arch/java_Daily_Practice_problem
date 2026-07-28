9fc3891183103aa5d15effb5148ad2f1b97a61ea
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-28 22:50:36 +0530

    Create print_no_20 to1.java

---
+++ print_no_20 to1.java
@@ -0,0 +1,11 @@
+public class print_no_20_1 {
+    public static void main(String[] args) {
+        int i = 20;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+    }
+
+    
+}
