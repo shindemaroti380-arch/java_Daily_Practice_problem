0b8f203d1b7d1b23c12bd24c2f1d8517c10b7b34
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-28 23:08:36 +0530

    Create do_while_loop.java

---
+++ do_while_loop.java
@@ -0,0 +1,10 @@
+ublic class do_while_loop {
+    public static void main(String[] args) {
+        int v = 0;
+        do{
+            System.out.println(v);
+            v++;
+        }while(v<=10);
+    }
+    
+}
