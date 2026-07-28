33e9850ff30285300eb4104cdac1bf575721edf6
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-28 23:15:25 +0530

    Create print_even_number.java

---
+++ print_even_number.java
@@ -0,0 +1,11 @@
+public class print_even_number {
+    public static void main(String[] args) {
+        int i = 2;
+        while(i<=100){
+            System.out.println(i);
+            i+= 2;
+
+        }
+    }
+    
+}
