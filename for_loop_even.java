184ddbde8d7542028be891dff35174a30daf7dd7
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-29 13:21:23 +0530

    Create for_loop_even.java

---
+++ for_loop_even.java
@@ -0,0 +1,10 @@
+public class for_loop_even {
+    public static void main(String[] args){
+        
+        for(int i = 2;i<=50;i++)
+            System.out.println(2 *i);
+        }
+
+    }
+    
+
