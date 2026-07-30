b34b49702525d28584a1ef291e4f2fc0e957becc
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-30 23:23:15 +0530

    Create multiplication_table_addition.java

---
+++ multiplication_table_addition.java
@@ -0,0 +1,15 @@
+
+public class multiplication_table_addition {
+    public static void main(String[] args){
+        int sum = 0;
+        int i = 8;
+        for(i = 1;i<=10;i++){
+            sum = sum + (i*8);
+           // System.out.println(sum);
+        }
+        System.out.println(sum);
+    }
+
+    
+}
+
