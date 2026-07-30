94a22641fe0b09b027bfd533dbf85b0a56686bbd
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-30 23:13:27 +0530

    Create first_even_natural_numbers.java

---
+++ first_even_natural_numbers.java
@@ -0,0 +1,13 @@
+public class first_even_numbers {
+    public static void main(String[] args){
+        int sum = 0;
+        int i = 10;
+        
+        for(i = 0;i<5;i++){
+            sum = sum + (2*i);
+        }
+        System.out.print(sum);
+
+    }
+    
+}
