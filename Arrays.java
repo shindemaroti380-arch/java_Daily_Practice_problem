2d8c1c808d4d079a304b115406b81dd1ae3b4d13
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-30 22:41:57 +0530

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class arrays {
+    public static void main(String[] args){
+        int[] marks = new int[5];
+        marks[0]= 50;
+        marks[1]= 70;
+        marks[2]= 80;
+        marks[3]= 90;
+        marks[4]= 40;
+        System.out.println(marks[4]);
+
+    }
+    
+}
