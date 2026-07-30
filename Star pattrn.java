243b06f6b3c88600bb7cdfaa5734f67f69b939ae
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-30 23:01:38 +0530

    Create Star pattrn.java

---
+++ Star pattrn.java
@@ -0,0 +1,16 @@
+public class star_pattern {
+    public static void main(String[] args){
+        
+        for( int i =1;i<=4;i++){
+            for(int j =0;j<i;j++)
+           
+                System.out.print("*");
+                System.out.print(" \n");
+
+
+            }
+
+        }
+    }
+    
+
