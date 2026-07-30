c2f70c5f66c2d668a7601273db1fb32691de8d78
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-30 23:27:52 +0530

    Create reverse_multiplication_table.java

---
+++ reverse_multiplication_table.java
@@ -0,0 +1,9 @@
+public class revers_multipication_table {
+    public static void main(String[] args){
+        int i=8;
+        for(i = 10;i>=1;i--){
+            System.out.println(8*i);
+        }
+    }
+    
+}
