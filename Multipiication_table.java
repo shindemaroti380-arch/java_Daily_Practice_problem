1eafe7ee9081baea7b1b0b5689845adac263995c
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-30 23:17:33 +0530

    Create Multipiication_table.java

---
+++ Multipiication_table.java
@@ -0,0 +1,9 @@
+public class multiplication_table {
+    public static void main(String[] args) {
+        int i = 8;
+        for( i = 1;i<=10;i++){
+            System.out.println(8*i);
+        }
+    }
+    
+}
