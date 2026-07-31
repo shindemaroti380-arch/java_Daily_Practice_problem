08072a6aa643adaa19950dd232669995b802296f
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-31 16:33:03 +0530

    Create for_loop_used.java

---
+++ for_loop_used.java
@@ -0,0 +1,10 @@
+public class arrays_for_loop_used {
+    public static void main(String[] args){
+        int[] marks = {90,53,80,70,80};
+        System.out.println(marks.length);
+        for(int i = 0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
