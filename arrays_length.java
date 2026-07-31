0f7ecb9b68a104fbae9c804a0996fb0aadd476d1
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-31 16:14:43 +0530

    Create arrays_length.java

---
+++ arrays_length.java
@@ -0,0 +1,9 @@
+public class arrays_length {
+    public static void main(String[] args){
+        int[] marks = {98,87,73,64,89,56};
+        System.out.println(marks.length);
+        }
+
+    }
+    
+
