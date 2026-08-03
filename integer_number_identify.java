0a8ac9168eb60244d78d64b330196d49891b3caf
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-08-03 23:03:28 +0530

    Create integer_number_identify.java

---
+++ integer_number_identify.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class chw_practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+         int num = sc.nextInt();
+         System.out.println("the number is integer:  ");
+         
+        
+        
+
+
+
+    }
+
+    
+}
